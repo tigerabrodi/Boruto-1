4f79233897784ee5b624a3078dcd44a8772415a0
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-09 19:36:01 +0200

    theme colors for  toaster

---
+++ src/lib/theme.ts
@@ -0,0 +1,22 @@
+export const ToastOptions = {
+  duration: 3000,
+  style: {
+    fontSize: '.9rem',
+    borderRadius: '0.5rem',
+    boxShadow: '0 3px 5px rgba(0, 0, 0, .1)',
+  },
+  success: {
+    style: {
+      marginTop: '100px',
+      backgroundColor: '#fff',
+      border: `0.1rem solid #eee`,
+    },
+  },
+  error: {
+    style: {
+      marginTop: '100px',
+      backgroundColor: '#fff',
+      border: `0.1rem solid #eee`,
+    },
+  },
+}
