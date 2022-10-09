d1c08016dbf04435f95bbbde064e3657a3114309
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-09 17:52:18 +0200

    close menu

--- src/components/header/menu/unauthenticated.tsx
+++ src/components/header/menu/unauthenticated.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 import { Link } from 'react-router-dom'
+import { useMenuContext } from '../../../context/MenuContext'
 import { DEFAULT_AVATAR } from '../avatar'
 
 export function Unauthenticated() {
+  const { setIsOpen } = useMenuContext()
+
   return (
-    <div className="absolute right-[30px] top-[102px] bg-white p-[30px] rounded-[5px] border border-border shadow-md">
+    <div
+      role="nav"
+      aria-label="unauthenticated menu"
+      className="absolute right-[30px] top-[102px] bg-white p-[30px] rounded-[5px] border border-border shadow-md"
+    >
       <img src={DEFAULT_AVATAR} alt="" className="w-20 mb-[15px] mx-auto" />
       <p
         className="w-[250px] text-[24px] font-semibold text-dark  mb-[15px] center"
@@ -19,12 +26,14 @@ export function Unauthenticated() {
 
       <div className="flex justify-between">
         <Link
+          onClick={() => setIsOpen(false)}
           className=" transition ease-in-out duration-200 py-[10px] px-[30px] bg-blue rounded-[30px] text-white hover:bg-hoverFilled"
           to="/signup"
         >
           Sign up
         </Link>
         <Link
+          onClick={() => setIsOpen(false)}
           className=" transition ease-in-out duration-200 py-[10px] px-[30px] rounded-[30px] text-blue border border-blue hover:bg-hoverOutlined"
           to="/signin"
         >
