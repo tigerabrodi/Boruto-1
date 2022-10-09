09fce07273872614f3bd24d50f4ac119b17a8f40
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-09 19:35:19 +0200

    implementing Toaster, loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -5,34 +5,41 @@ import { Header } from './components'
 import { Route, Routes } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext'
 import { MenuContextProvider } from './context/MenuContext'
+import { Toaster } from 'react-hot-toast'
+import { ToastOptions } from './lib/theme'
+import { LoadingSpinner } from './components/spinner'
 
 const Feed = lazy(() => import('./pages/feed/feed'))
 const Signup = lazy(() => import('./pages/signup/signup'))
 
 export function App() {
   return (
-    <AuthContextProvider>
-      <MenuContextProvider>
-        <Header />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Suspense fallback="loading...">
-                <Feed />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/signup"
-            element={
-              <Suspense fallback="loading...">
-                <Signup />
-              </Suspense>
-            }
-          />
-        </Routes>
-      </MenuContextProvider>
-    </AuthContextProvider>
+    <div>
+      <Toaster position="top-center" toastOptions={ToastOptions} />
+      <LoadingSpinner />
+      <AuthContextProvider>
+        <MenuContextProvider>
+          <Header />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Suspense fallback={<LoadingSpinner />}>
+                  <Feed />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <Suspense fallback={<LoadingSpinner />}>
+                  <Signup />
+                </Suspense>
+              }
+            />
+          </Routes>
+        </MenuContextProvider>
+      </AuthContextProvider>
+    </div>
   )
 }
