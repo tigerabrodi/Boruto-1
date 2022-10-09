24b951daee29815817057707dc6c384aa0277028
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-09 19:35:31 +0200

    creating a loading spinner

---
+++ src/components/spinner/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { CgSpinner } from 'react-icons/cg'
+import { useLoadingStore } from '../../lib/store'
+
+import './style.css'
+
+export const LoadingSpinner = () => {
+  const { status } = useLoadingStore()
+  return status === 'loading' ? (
+    <div role="alert" aria-label="loading" className="spinner">
+      <CgSpinner className="spin " />
+    </div>
+  ) : null
+}
