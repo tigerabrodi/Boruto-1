bd01e1230cf6f4e94f2fd462cfc24c28c1ff413f
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-09 17:36:07 +0200

    implementing react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import { lazy, Suspense } from 'react'
 import { Header } from './components'
+import { Route, Routes } from 'react-router-dom'
 import { AuthContextProvider } from './context/AuthContext'
 import { MenuContextProvider } from './context/MenuContext'
 
+const Feed = lazy(() => import('./pages/feed/feed'))
+const Signup = lazy(() => import('./pages/signup/signup'))
+
 export function App() {
   return (
-    <MenuContextProvider>
-      <AuthContextProvider>
+    <AuthContextProvider>
+      <MenuContextProvider>
         <Header />
-      </AuthContextProvider>
-    </MenuContextProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback="loading...">
+                <Feed />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <Suspense fallback="loading...">
+                <Signup />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </MenuContextProvider>
+    </AuthContextProvider>
   )
 }
---
+++ src/pages/signup/signup.tsx
@@ -0,0 +1,5 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+export default function Signup() {
+  return <div>signup</div>
+}
