249791680d3baa239404aac12641020fa8a5f77e
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-09 19:24:08 +0200

    create color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,8 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     colors: {
+      error: '#FF3433',
+      success: '#009441',
       blue: '#3466f6',
       hoverOutlined: '#3464f61f',
       hoverFilled: '#507cff',
