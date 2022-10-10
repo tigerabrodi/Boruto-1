8939dcafbd2c1acbeaf9a6efa0f5a3cef3c844f3
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-10 07:18:08 +0200

    test: sign up and create account

--- cypress/e2e/sign-up-flow.cy.ts
+++ cypress/e2e/sign-up-flow.cy.ts
@@ -1,16 +1,16 @@
-// import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
 
-// const DEMO_AVATAR = 'demo-avatar.jpg'
+const DEMO_AVATAR = 'demo-avatar.jpg'
 
-// const newUser = {
-//   username: faker.internet.userName(),
-//   email: faker.internet.email(),
-//   password: faker.internet.password(),
-//   fullName: faker.name.fullName(),
-//   age: '21',
-//   location: 'Tokyo, Japan',
-//   bio: faker.random.words(7),
-// }
+const user = {
+  username: faker.internet.userName(),
+  email: faker.internet.email(),
+  password: 'demonslayer123',
+  fullName: faker.name.fullName(),
+  age: '21',
+  location: 'Tokyo, Japan',
+  bio: faker.random.words(7),
+}
 
 it('Sign up and create an account', () => {
   cy.visit('/')
@@ -29,4 +29,22 @@ it('Sign up and create an account', () => {
   cy.findByRole('button', { name: 'unauthenticated nav menu' }).click()
   cy.findByRole('nav', { name: 'unauthenticated menu' }).should('be.visible')
   cy.findByRole('link', { name: 'Sign up' }).click()
+
+  // User signs up
+  cy.findByLabelText('Username').type(user.username)
+  cy.findByLabelText('Email').type(user.email)
+  cy.findByLabelText('Password').type(user.password)
+  cy.findByLabelText('Confirm Password').type(user.password)
+  cy.findByRole('button', { name: 'Sign up' }).click()
+
+  //  User creates account
+  cy.findByRole('heading', { level: 1, name: 'Create your account' }).should(
+    'be.visible'
+  )
+  cy.findByLabelText('Choose File').attachFile(DEMO_AVATAR)
+  cy.findByLabelText('Full Name *').type(user.fullName)
+  cy.findByLabelText('Age *').type(user.age)
+  cy.findByLabelText('Location *').type(user.location)
+  cy.findByLabelText('Biography').type(user.bio)
+  cy.findByRole('button', { name: 'Done' }).click()
 })
