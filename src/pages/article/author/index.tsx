cf1f21bde3e01197e931fa8a2512e08ff4045082
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-10 22:09:35 +0200

    fetching article "author" and render it

---
+++ src/pages/article/author/index.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import { Link } from 'react-router-dom'
+
+type AuthorProps = {
+  profileId: string
+  avatarUrl: string
+  fullname: string
+  PIN: string
+  uid: string
+}
+export function Author({
+  avatarUrl,
+  fullname,
+  PIN,
+  uid,
+  profileId,
+}: AuthorProps) {
+  return (
+    <>
+      {uid === PIN && (
+        <>
+          <img
+            src={avatarUrl}
+            alt=""
+            className="w-[55px] rounded-[50%] mr-[20px]"
+          />
+          <Link
+            className=" text-[18px]   mr-[30px] hover:underline"
+            to={`/profile/${profileId}`}
+          >
+            {fullname}
+          </Link>
+        </>
+      )}
+    </>
+  )
+}
