f87b7d97133dec462ccee2360f4c7a6c57139ab2
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-11 12:55:03 +0200

    change toast styling (little bit)

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -2,21 +2,21 @@ export const ToastOptions = {
   duration: 3000,
   style: {
     fontSize: '.9rem',
-    borderRadius: '0.5rem',
-    boxShadow: '0 3px 5px rgba(0, 0, 0, .1)',
+    borderRadius: '0.3rem',
+    boxShadow: '0 2px 5px rgba(0, 0, 0, .2)',
   },
   success: {
     style: {
-      marginTop: '100px',
+      marginTop: '90px',
       backgroundColor: '#fff',
-      border: `0.1rem solid #eee`,
+      border: `0.1rem solid #ddd`,
     },
   },
   error: {
     style: {
-      marginTop: '100px',
+      marginTop: '90px',
       backgroundColor: '#fff',
-      border: `0.1rem solid #eee`,
+      border: `0.1rem solid #ddd`,
     },
   },
 }
