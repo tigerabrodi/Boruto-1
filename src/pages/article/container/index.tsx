8c1e12cd05bddca9576ccac9c47a66c657dc19fe
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-11 12:34:32 +0200

    textarea (only for authenticated users) component

--- src/pages/article/container/index.tsx
+++ src/pages/article/container/index.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { useAuthContext } from '../../../context'
 import { Container, Textarea } from '../index'
 export function CommentsContainer() {
+  const { user } = useAuthContext()
   return (
-    <div className="mx-auto">
-      <Container />
-      <Textarea />
+    <div className="mx-auto mb-[100px]">
+      {user?.uid ? <Textarea /> : <Container />}
     </div>
   )
 }
--- src/pages/article/container/textarea.tsx
+++ src/pages/article/container/textarea.tsx
@@ -1,5 +1,52 @@
 /* eslint-disable react/react-in-jsx-scope */
 
+import { doc, DocumentData, onSnapshot } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { useAuthContext } from '../../../context'
+import { firebaseDb } from '../../../lib'
+
 export function Textarea() {
-  return <div>textarea</div>
+  const [profile, setProfile] = useState<DocumentData>()
+
+  const { user } = useAuthContext()
+
+  const avatarDocumentReference = doc(firebaseDb, `users/${user?.uid}`)
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(avatarDocumentReference, (doc) => {
+      setProfile(doc.data())
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+  return (
+    <div className="w-[800px] bg-white border border-border rounded-[8px]">
+      {profile && (
+        <div className="flex items-center p-[30px] border-b border-border">
+          <img
+            src={profile.avatarUrl}
+            alt="Your profile picture"
+            className="rounded-[50%] w-[60px]"
+          />
+          <div className="ml-[15px]">
+            <p className="font-semibold">{profile.fullname}</p>
+            <p className="text-darkGrey ] pt-[3px]">
+              {profile.bio.substr(0, 85) + '...'}
+            </p>
+          </div>
+        </div>
+      )}
+      <label htmlFor="Comment"></label>
+      <textarea
+        name="Comment"
+        id="Comment"
+        className="w-[100%] h-[280px] resize-none p-[30px] text-[18px] leading-[2] border-b border-border"
+      />
+      <button className="my-[30px] mx-auto flex justify-center  text-blue border border-blue py-[6px] w-[120px] rounded-[30px] hover:bg-hoverOutlined transition ease-in-out duration-200 ">
+        Post
+      </button>
+    </div>
+  )
 }
