04c0e19a1e6d2a891484aab2a558af2703142cd4
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-11 13:05:34 +0200

    fetching and rendering amout of comments

--- src/pages/article/buttons/CommentButton.tsx
+++ src/pages/article/buttons/CommentButton.tsx
@@ -1,10 +1,41 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { collection, CollectionReference, onSnapshot } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
 import { IoChatbubblesOutline } from 'react-icons/io5'
+import { ButtonsProps } from '.'
+import { CommentType, firebaseDb } from '../../../lib'
 
-export function CommentButton() {
+export function CommentButton({ articleId }: ButtonsProps) {
+  const [comments, setComments] = useState<CommentType[]>([])
+
+  const commentsCollectionReference = collection(
+    firebaseDb,
+    `articles/${articleId}/comments`
+  ) as CollectionReference<CommentType>
+
+  useEffect(() => {
+    const getComments = async () =>
+      onSnapshot(commentsCollectionReference, (snapshot) => {
+        return setComments(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        )
+      })
+
+    return () => {
+      getComments()
+    }
+  }, [firebaseDb, articleId])
   return (
-    <button aria-label="comment button" className="text-[35px]  text-darkGrey">
+    <button
+      aria-label="comment button"
+      className="text-[36px]  text-darkGrey  text-darkGrey mb-[50px] flex items-center"
+    >
       <IoChatbubblesOutline />
+      {comments.length > 0 && (
+        <span className="text-[25px] mb-[-8px] ml-[10px]">
+          {comments.length}
+        </span>
+      )}
     </button>
   )
 }
