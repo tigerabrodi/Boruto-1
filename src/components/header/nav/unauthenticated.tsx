4acf73d81860d7df03efc44d21c477607b3302c0
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-14 10:50:01 +0200

    recreate unauthenticated nav menu

---
+++ src/components/header/nav/unauthenticated.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable react/react-in-jsx-scope */
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { DEFAULT_AVATAR } from '../../../lib'
+
+export function Unauthenticated() {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-meu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <img src={DEFAULT_AVATAR} alt="" className="w-12" />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        sx={{ top: '18px' }}
+      >
+        <img
+          src={DEFAULT_AVATAR}
+          alt=""
+          className="w-20 mb-[15px] mx-auto mb-[30px] mt-[20px]"
+        />
+        <p
+          className="w-[250px] text-[24px] font-semibold text-dark  mb-[10px] text-center  mx-[10px]"
+          tabIndex={0}
+        >
+          Sign up or sign in to your Boruto account.
+        </p>
+        <p className="text-darkGrey mb-[20px] px-[15px] mx-[10px]" tabIndex={0}>
+          Takes less than a few seconds.
+        </p>
+
+        <div className="flex justify-between   mx-[15px] mb-[20px]">
+          <Link
+            onClick={handleClose}
+            className=" transition ease-in-out duration-200 py-[10px] px-[30px] bg-blue rounded-[30px] text-white hover:bg-hoverFilled"
+            to="/signup"
+          >
+            Sign up
+          </Link>
+          <Link
+            onClick={handleClose}
+            className=" transition ease-in-out duration-200 py-[10px] px-[30px] rounded-[30px] text-blue border border-blue hover:bg-hoverOutlined"
+            to="/signin"
+          >
+            Sign in
+          </Link>
+        </div>
+      </Menu>
+    </div>
+  )
+}
