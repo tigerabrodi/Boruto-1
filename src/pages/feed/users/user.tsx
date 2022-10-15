24a85ccb46f4fb3dc1d84cbde4b9164259ff9c32
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-15 12:12:50 +0200

    fetch user and render them

---
+++ src/pages/feed/users/user.tsx
@@ -0,0 +1,97 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import { collection, CollectionReference, onSnapshot } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import {
+  firebaseDb,
+  FollowType,
+  getDateWithTimestamp,
+  getTimestamp,
+  UserType,
+} from '../../../lib'
+import { useAuthContext } from '../../../context'
+import { Link } from 'react-router-dom'
+
+export function User({
+  pin,
+  bio,
+  age,
+  fullname,
+  location,
+  username,
+  avatarUrl,
+  profileId,
+  createdAt,
+}: UserType) {
+  const [followers, setFollowers] = useState<FollowType[]>([])
+  const followersCollectionReference = collection(
+    firebaseDb,
+    `users/${profileId}/followers`
+  ) as CollectionReference<FollowType>
+
+  useEffect(
+    () =>
+      onSnapshot(followersCollectionReference, (snapshot) =>
+        setFollowers(snapshot.docs.map((doc) => ({ ...doc.data() })))
+      ),
+    [firebaseDb, profileId]
+  )
+  return (
+    <div className="relative flex  bg-white w-[650px] mx-auto border border-border mt-[15px] p-[30px] rounded-[8px] ">
+      <p className="absolute text-lightGrey top-[10px] right-[10px] text-[14px]">
+        Since: {getDateWithTimestamp(getTimestamp(createdAt))}
+      </p>
+      <div>
+        <Link to={`/profiles/${profileId}`}>
+          <img
+            src={avatarUrl}
+            alt="Your profile picture"
+            className="w-[100px] rounded-[50%] hover:opacity-80 transition ease-in-out duration-200 mb-[10px]"
+          />
+        </Link>
+        <Link
+          to={`/profiles/${profileId}`}
+          tabIndex={0}
+          className="text-lightGrey text-[14px] text-center hover:underline"
+          aria-label={`Your username`}
+        >
+          @{username}
+        </Link>
+      </div>
+      <div className=" ml-[30px] self-center">
+        <p className="rounded-[3px] text-[14px] bg-border inline-block px-[8px] py-[2px]">
+          <span className="font-semibold mr-[3px]">{followers.length}</span>{' '}
+          Followers
+        </p>
+
+        <div className="flex items-center mt-[8px]">
+          <p
+            tabIndex={0}
+            className=" font-semibold text-base "
+            aria-label={`Your full name`}
+          >
+            {fullname}
+          </p>
+          <p className="text-[22px] mx-[10px]">·</p>
+          <p className=" font-semibold text-base ">{age} y/o</p>
+          <p className="text-[22px] mx-[10px]">·</p>
+          <p
+            tabIndex={0}
+            className=" font-semibold text-base "
+            aria-label={`Your full location`}
+          >
+            {location}
+          </p>
+        </div>
+
+        <p
+          tabIndex={0}
+          className="mt-[5px]  text-base "
+          aria-label={`Your full biography`}
+        >
+          {bio}
+        </p>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/feed/users/users.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/react-in-jsx-scope */
+
+import { collection, CollectionReference, onSnapshot } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { User } from '..'
+import { firebaseDb, UserType } from '../../../lib'
+
+export function Users() {
+  const [users, setUsers] = useState<UserType[]>([])
+
+  const usersCollectionReference = collection(
+    firebaseDb,
+    'users'
+  ) as CollectionReference<UserType>
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(usersCollectionReference, (snapshot) => {
+      setUsers(
+        snapshot.docs.map((doc) => ({ ...doc.data(), profileId: doc.id }))
+      )
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [firebaseDb])
+  return (
+    <div className="mt-[15px]">
+      {users.map(
+        ({
+          pin,
+          bio,
+          age,
+          fullname,
+          location,
+          username,
+          avatarUrl,
+          profileId,
+          createdAt,
+        }) => {
+          return (
+            <User
+              pin={pin}
+              bio={bio}
+              age={age}
+              key={profileId}
+              fullname={fullname}
+              location={location}
+              username={username}
+              avatarUrl={avatarUrl}
+              createdAt={createdAt}
+              profileId={profileId}
+            />
+          )
+        }
+      )}
+    </div>
+  )
+}
