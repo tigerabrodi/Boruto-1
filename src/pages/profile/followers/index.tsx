04c1447431dbd25b56be773d20c3ceea045bde88
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-15 12:12:33 +0200

    fix: spelling

--- src/pages/profile/followers/index.tsx
+++ src/pages/profile/followers/index.tsx
@@ -2,31 +2,32 @@
 
 import { collection, CollectionReference, onSnapshot } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
-import { firebaseDb, LikeType } from '../../../lib'
+import { firebaseDb, FollowType } from '../../../lib'
 
 type FollowersProps = {
   profileId: string | undefined
 }
 
 export function Followers({ profileId }: FollowersProps) {
-  const [likes, setLikes] = useState<LikeType[]>([])
+  const [followers, setFollowers] = useState<FollowType[]>([])
 
-  const likeCollectionReference = collection(
+  const followersCollectionReference = collection(
     firebaseDb,
     `users/${profileId}/followers/`
-  ) as CollectionReference<LikeType>
+  ) as CollectionReference<FollowType>
 
   useEffect(
     () =>
-      onSnapshot(likeCollectionReference, (snapshot) =>
-        setLikes(snapshot.docs.map((doc) => ({ ...doc.data() })))
+      onSnapshot(followersCollectionReference, (snapshot) =>
+        setFollowers(snapshot.docs.map((doc) => ({ ...doc.data() })))
       ),
     [firebaseDb, profileId]
   )
 
   return (
     <p className="rounded-[3px] text-[14px] bg-border inline-block px-[8px] py-[2px]">
-      <span className="font-semibold mr-[3px]">{likes.length}</span> Followers
+      <span className="font-semibold mr-[3px]">{followers.length}</span>{' '}
+      Followers
     </p>
   )
 }
